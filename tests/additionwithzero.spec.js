255d14f5aa11d305a422f972c213289df0683ae2
Alberto2708 <mentadoreyescarlosalberto@gmail.com>
2025-02-24 08:51:28 -0600

    uwu

--- tests/additionwithzero.spec.js
+++ tests/additionwithzero.spec.js
@@ -34,7 +34,7 @@ vars = {}
     await driver.quit();
   })
   it('Addition with zero', async function() {
-    await driver.get("http://127.0.0.1:5500/")
+    await driver.get("http://127.0.0.1:8000/")
     await driver.manage().window().setRect({ width: 813, height: 912 })
     await driver.findElement(By.id("num1")).click()
     await driver.findElement(By.id("num1")).sendKeys("0")
